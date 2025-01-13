3f64870eb8c36e6bbae2752fcd69d6f3ef8845b4
CecinestpasJonatan <jonatlin@kth.se>
2025-01-13 23:17:07 +0100

    Added power kwarg to lr scheduler

--- Mujoco_models/Mujoco_experiment.py
+++ Mujoco_models/Mujoco_experiment.py
@@ -6,25 +6,19 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-def linear_schedule(initial_value):
+def linear_schedule(initial_value, power=2):
     def func(progress_remaining):
-        return progress_remaining * initial_value
+        return (progress_remaining**power) * initial_value
     return func
 
-# Parallel environments
-vec_env = gym.make('Hopper-v5', render_mode='human')
+vec_env = gym.make('Hopper-v5')
+vec_env = make_vec_env(vec_env, n_envs=8)
 
-policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=dict(pi=[512, 512], vf=[512, 512]))
+policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=dict(pi=[512, 512], vf=[512]))
 
-model = PPO("MlpPolicy", vec_env, verbose=1, device="cpu", learning_rate=linear_schedule(0.001))
+model = PPO("MlpPolicy", vec_env, verbose=1, device="cpu", learning_rate=linear_schedule(0.001, ))
 model.learn(total_timesteps=250000)
-# model.save("ppo_cartpole")
 
-# del model # remove to demonstrate saving and loading
-
-# model = PPO.load("ppo_cartpole")
-
-# obs = vec_env.reset()
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = vec_env.step(action)
